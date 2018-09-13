9f9d5432f69524a689da999c9a48ea88127f450e
james gill <james@jgdev.me>
2018-09-13 07:59:34 -0700

    backend making requests to api for currency values in USD and requests to exchangerate

--- server/index.js
+++ server/index.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const keys = require('./api_keys');
 
 const app = express();
 
+
 let port = 5000 || process.env.PORT
 
-app.get('/api/coins', (req, res) => {
-  res.send({message: "I'm riach biatch"});
+// Get the exchange rate between a real currency and a cryptocurrency
+app.get('/api/exchangerate', (req, res) => {
+  const crypto = req.query.crypto;
+  const currency = req.query.currency;
+
+  if (crypto && currency) {
+  axios.get(`https://rest.coinapi.io/v1/exchangerate/${crypto}/${currency}?apikey=${keys.coinApi}`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 });
+
+// const getAllCoinsInUSD = () => {
+//   const cryptoCoins = ['BTC', 'ETH'];
+//   const cryptoCurrencyValues = [];
+
+//   cryptoCoins.forEach((coin) => {
+//     axios.get(`https://rest.coinapi.io/v1/exchangerate/${coin}/USD?apikey=${keys.coinApi}`)
+//     .then((response) => {
+//       console.log(response.data);
+//       cryptoCurrencyValues.push(response.data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+//   });
+
+//   console.log(cryptoCurrencyValues);
+// }
+
+// getAllCoinsInUSD();
+
+// [0] { time: '2018-09-13T14:57:46.5712577Z',
+// [0]   asset_id_base: 'ETH',
+// [0]   asset_id_quote: 'USD',
+// [0]   rate: 202.64736524900775 }
+// [0] { time: '2018-09-13T14:57:48.9619488Z',
+// [0]   asset_id_base: 'BTC',
+// [0]   asset_id_quote: 'USD',
+// [0]   rate: 6522.570752358936 }
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`);
--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-// const API_KEY = '7A5F35CC-F77B-4636-8EE8-B799158AC34F';
-
 class App extends Component {
   render() {
-    // axios.get(`https://rest.coinapi.io/v1/exchangerate/BTC?apikey=${API_KEY}`)
-    // .then((response) => {
-    //   console.log(response);
-    // })
     return (
       <div className="App">
         {/* <header className="App-header">
