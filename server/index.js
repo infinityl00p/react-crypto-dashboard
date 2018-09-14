d9246d87052be05821ebbbd7d7f7f53352544b09
james gill <james@jgdev.me>
2018-09-14 13:46:22 -0700

    cron successfully adding historical coin rates and current coin rates at differing frequencies. Cron also wiping DB at 11:59:50 am everyday

--- server/index.js
+++ server/index.js
@@ -25,16 +25,6 @@ app.get('/api/coins', (req, res) => {
 app.get('/api/exchangerate', (req, res) => {
   const crypto = req.query.crypto;
   const currency = req.query.currency;
-
-  if (crypto && currency) {
-  axios.get(`https://rest.coinapi.io/v1/exchangerate/${crypto}/${currency}?apikey=${keys.coin_api}`)
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
 });
 
 app.get('/api/coins/historical', (req, res) => {
@@ -50,19 +40,6 @@ app.get('/api/coins/historical', (req, res) => {
       //console.log(error);
     })
 });
-
-var starttime = new Date();
-// Get the iso time (GMT 0 == UTC 0)
-var isotime = new Date((new Date(starttime)).toISOString() );
-// getTime() is the unix time value, in milliseconds.
-// getTimezoneOffset() is UTC time and local time in minutes.
-// 60000 = 60*1000 converts getTimezoneOffset() from minutes to milliseconds.
-var fixedtime = new Date(isotime.getTime()-(starttime.getTimezoneOffset()*60000));
-// toISOString() is always 24 characters long: YYYY-MM-DDTHH:mm:ss.sssZ.
-// .slice(0, 19) removes the last 5 chars, ".sssZ",which is (UTC offset).
-// .replace('T', ' ') removes the pad between the date and time.
-var formatedMysqlString = fixedtime.toISOString().slice(0, 19).replace('T', ' ');
-console.log( formatedMysqlString );
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`);
--- server/services/historicalCoinRates.js
+++ server/services/historicalCoinRates.js
@@ -1,14 +1,49 @@
-module.exports = {
-  getHistoricalCoinRates: (coins, time_start) => {
+const axios = require('axios');
+const keys = require('../config/api_keys');
+const dbCon = require('../config/db_config').dbCon;
+
+init = (coins, yesterday) => {
+  wipeCoinTable();
+  getHistoricalCoinRates(coins, yesterday);
+}
+
+getHistoricalCoinRates = (coins, yesterday) => {
     const symbol_id = 'BITSTAMP_SPOT_BTC_USD';
     const period_id = '1DAY';
 
-    axios.get(`https://rest.coinapi.io/v1/ohlcv/${symbol_id}/history?period_id=${period_id}&time_start=${time_start}`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
+    coins.forEach((coin) => {
+      axios.get(`https://rest.coinapi.io/v1/exchangerate/${coin}/USD?time=${yesterday}&apikey=${keys.coin_api}`)
+        .then((response) => {
+          // console.log(response.data);
+          insertHistoricalCoinRates(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    });
 }
+
+wipeCoinTable = () => {
+  const queryString ='DELETE FROM coins';
+
+  dbCon.connect((error) => {
+    dbCon.query(queryString, function (err, result) { })
+  });
+}
+
+insertHistoricalCoinRates = (data) => {
+  const base = data.asset_id_base;
+  const quote = data.asset_id_quote;
+  const rate = data.rate;
+  const time = data.time;
+  const historical = true;
+
+  const queryString = `INSERT INTO coins (base, quote, rate, time, historical) VALUES (?, ?, ?, ?, ?)`;
+
+  dbCon.connect((error) => {
+    dbCon.query(queryString, [base, quote, rate, time, historical], function (err, result) { })
+  });
+
+}
+
+module.exports = { init, getHistoricalCoinRates, wipeCoinTable, insertHistoricalCoinRates }
