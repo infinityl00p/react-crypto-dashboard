f259eae90b90895f2af8b3586cc1e02d901598fc
james gill <james@jgdev.me>
2018-09-19 21:50:00 -0700

    Begin currency conversion tool

--- src/components/DashboardView/ConversionView.js
+++ src/components/DashboardView/ConversionView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Card from '../Reusable/Card';
 
 class ConversionView extends Component {
   render() {
@@ -6,6 +7,11 @@ class ConversionView extends Component {
       <div className="conversion-view">
         <h2>Currency Conversion</h2>
         <div className="view-container">
+        <Card>
+          <div className="content--center">
+Laboris exercitation commodo nulla occaecat excepteur non occaecat ullamco laboris reprehenderit. Reprehenderit incididunt aliqua aliqua ad amet nisi Lorem. Sint nisi tempor fugiat culpa consectetur elit velit cillum pariatur sint anim nisi ea exercitation. Ex ullamco adipisicing excepteur exercitation tempor incididunt excepteur velit duis est minim. Tempor proident do sit sit aliquip reprehenderit minim magna ut incididunt exercitation.
+          </div>
+        </Card>
         </div>
       </div>
     );
